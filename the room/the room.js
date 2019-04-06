b287a50808ea20f758f64e52e6f9075fda0e01fe
thanhluan208 <skthunte33@gmail.com>
2019-04-06 19:08:20 +0700

    m

---
+++ the room/the room.js
@@ -0,0 +1,16 @@
+var bottle = document.getElementById("bottle");
+bottle.addEventListener('click',function(e){
+    console.log("heol");
+});
+var mirror = document.getElementById("mirror");
+mirror.addEventListener('click',function(e){
+    console.log("hello");
+});
+var drawers = document.getElementById("drawers");
+drawers.addEventListener('click',function(e){
+    console.log("halo");
+});
+var door = document.getElementById("door");
+door.addEventListener('click',function(){
+    console.log("door");
+});
