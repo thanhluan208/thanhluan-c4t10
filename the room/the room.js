a9ca0f44ebef323865a5a053f2304fdf42459fda
thanhluan208 <skthunte33@gmail.com>
2019-04-06 21:31:20 +0700

    room

--- the room/the room.js
+++ the room/the room.js
@@ -1,7 +1,20 @@
-var bottle = document.getElementById("bottle");
-bottle.addEventListener('click',function(e){
-    console.log("heol");
+var table = document.getElementById("table");
+var background = document.getElementById("back-ground");
+var back = document.getElementById('back');
+
+// Initial:
+back.style.display = 'none';
+
+table.addEventListener('click',function(e){
+    back.style.display = 'block';
+    background.setAttribute('class', 'scaleTable');
+    addHtml = `
+        <div id="bottle"></div>
+    `;
+    background.insertAdjacentHTML('afterbegin', addHtml);
+    backButton();
 });
+
 var mirror = document.getElementById("mirror");
 mirror.addEventListener('click',function(e){
     console.log("hello");
@@ -14,3 +27,11 @@ var door = document.getElementById("door");
 door.addEventListener('click',function(){
     console.log("door");
 });
+
+function backButton() {
+    var back = document.getElementById("back");
+    back.addEventListener('click', function(e){
+        background.removeAttribute('class', 'scaleTable');
+        back.style.display = "none";
+    })
+}
