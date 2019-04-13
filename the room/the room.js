ac719ffe5a26b68bbc8e5a8ff1d59c51740a3704
thanhluan208 <skthunte33@gmail.com>
2019-04-13 17:49:33 +0700

    m

--- the room/the room.js
+++ the room/the room.js
@@ -10,25 +10,96 @@ table.addEventListener('click',function(e){
     background.setAttribute('class', 'scaleTable');
     addHtml = `
         <div id="bottle"></div>
+        <div id = "candle"></div>
+        <div id = "broken_glass"></div>
     `;
     background.insertAdjacentHTML('afterbegin', addHtml);
-    backButton();
+    var bottle = document.getElementById("bottle");
+    bottle.addEventListener('click', function (e) {
+        addHtml = `
+        <pre id="messagebottle">The bottle is now filled
+        with water. Drink it?</pre>
+        `;
+        background.insertAdjacentHTML('afterbegin', addHtml);
+        var messagebottle = document.getElementById("messagebottle");
+        messagebottle.addEventListener('click', function(e) {
+            messagebottle.remove();
+        });
+    });
+    backButton1();
 });
 
 var mirror = document.getElementById("mirror");
 mirror.addEventListener('click',function(e){
     back.style.display = "block";
     background.setAttribute('class',"scaleMirror");
-    backButton();
+    backButton()
 });
 var drawers = document.getElementById("drawers");
 drawers.addEventListener('click',function(e){
-    console.log("halo");
+    addHtml = `
+    <div id=safelock>
+    <form>
+        <label for="pscd">Enter 8-digit passcode: </label>
+        <input type="number" id="pscd">
+        <input type="button" value="Unlock" id="unlock" />
+    </form>
+    </div>
+    `;
+    background.insertAdjacentHTML('afterbegin', addHtml);
+    var unlock = document.getElementById("unlock");
+    unlock.addEventListener('click', function (e) {
+        var confirmPassword = "68911111";
+        var password = document.getElementById("pscd").value;
+        if (password == confirmPassword) {
+            window.location="welcome.html";
+        }
+        else {
+            alert("Wrong passcode.");
+        }
+    });
 });
 var door = document.getElementById("door");
 door.addEventListener('click',function(){
     console.log("door");
 });
+
+var paper = document.getElementById("papers");
+paper.addEventListener('click',function(e){
+    addHtml = `
+        <div id="newspaper">
+        <img src="oldnewspaper.png" alt="Newspaper with some clue.">
+        </div>
+    `;
+    background.insertAdjacentHTML('afterbegin', addHtml);
+    var newspaper = document.getElementById("newspaper");
+    newspaper.addEventListener('click', function(e) {
+        newspaper.remove();
+    })
+})
+var box = document.getElementById("box");
+box.addEventListener("click",function(e){
+    addHtml = `
+        <div id="match">
+        <img src="RedTopSafety Matchbox.png" alt="Matchbox">
+        </div>
+    `;
+    background.insertAdjacentHTML('afterbegin', addHtml);
+    var match = document.getElementById("match");
+    match.addEventListener('click', function(e) {
+        match.remove();
+    })
+})
+
+function backButton1() {
+    var back = document.getElementById("back");
+    back.addEventListener('click', function(e){
+        background.removeAttribute('class', 'scaleTable');
+        back.style.display = "none";
+        bottle.remove();
+        messagebottle.remove();
+    })
+}
 
 function backButton() {
     var back = document.getElementById("back");
@@ -37,11 +108,3 @@ function backButton() {
         back.style.display = "none";
     })
 }
-var paper = document.getElementById("papers");
-paper.addEventListener('click',function(e){
-    console.log("paper");
-})
-var box = document.getElementById("box");
-box.addEventListener("click",function(e){
-    console.log("box");
-})
